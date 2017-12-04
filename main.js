cd6d3c37918744af7f518c4c2bc40373ea2b88a2
Joao Paulo <joaopaulogse@gmail.com>
2017-12-04 19:32:07 -0200

    primeiro commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const electron = require("electron")
+const { app, BrowserWindow,ipcMain, ipcRenderer } = require("electron");
+const menubar = require("menubar")
+// let mb = menubar(
+//     {
+//         dir:process.cwd(), 
+//         index:`file://${__dirname}/menu.html`,
+//         width:200,
+//         height:200
+//     }  
+// );
+
+let win;
+// mb.on('ready', ()=>{
+//     // console.log('menu')
+// })
+
+app.on('ready', ()=>{
+    const {height,width} = electron.screen.getPrimaryDisplay().workAreaSize
+    win = new BrowserWindow({width, height});
+    ipcMain.on('fullScreen-main', (event, args)=>{
+        if(!win.isFullScreen()){
+            win.setFullScreen(args.value)
+        }else{
+            win.setFullScreen(false);
+        }
+    })
+    win.loadURL(`file://${__dirname}/index.html`)
+})
+ipcMain.on("CtrlClock", (event, args)=>{
+    // if(args.type == "start")
+    console.log(args)
+    // console.log(event)
+    event.sender.send("page", {type:args.type});
+})
